d59e6943c7404ed49623575c633a42fa40c27d3d
whimsy <whimsy@sylph.box>
2025-05-17 14:08:25 -0500

    modified:   start_qemu_windows11.sh

--- start_qemu_windows11.sh
+++ start_qemu_windows11.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-# This script starts a Windows 11 VM in QEMU with basic networking and graphics support.
-
 DISK_IMAGE="/mnt/part7/windows11.qcow2"
+ISO_OPTIONS=""
+
+# If an ISO path is provided, add it to the QEMU options.
+if [ "$#" -ge 1 ]; then
+  ISO_PATH="$1"
+  ISO_OPTIONS="-cdrom $ISO_PATH -boot d"
+fi
 
 qemu-system-x86_64 \
   -enable-kvm \
@@ -10,6 +15,7 @@ qemu-system-x86_64 \
   -m 4G \
   -vga virtio \
   -drive file="$DISK_IMAGE",format=qcow2 \
+  $ISO_OPTIONS \
   -netdev user,id=net0 \
   -device e1000,netdev=net0 \
   -device virtio-balloon
